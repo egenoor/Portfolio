b6937275d72b2ac359c1285d23538c6fb7569010
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-09-26 16:51:49 +0000

    disabled host check on package.json

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     devServer:{
         contentBase: path.join(__dirname, "dist"),
         compress: false,
+	disableHostCheck: true,
         host: '0.0.0.0',
         publicPath: "/",
         historyApiFallback: true,
